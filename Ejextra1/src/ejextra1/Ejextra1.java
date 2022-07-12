3b0fe8da2ad89d8dc331c7c301cc05bd7f8eed34
Martin <juanmartinf7@hotmail.com>
2022-07-12 20:05:39 -0300

    EjExtra1

---
+++ Ejextra1/src/ejextra1/Ejextra1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejextra1;
+
+/**
+ *
+ * @author Flores
+ */
+public class Ejextra1 {
+
+    /**
+     * 1. Desarrollar una clase Cancion con los siguientes atributos: titulo y autor. Se deberá́definir
+además dos constructores: uno vacío que inicializa el titulo y el autor a cadenas vacías y
+otro que reciba como parámetros el titulo y el autor de la canción. Se deberán además
+definir los métodos getters y setters correspondientes.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
